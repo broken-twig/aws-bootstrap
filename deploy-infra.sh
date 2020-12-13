1fc2b263e84c2bfc1526013568bf53da33d73394
Andrew Lindsay <andrew.lindsay@gmail.com>
2020-12-13 19:27:01 +1100

    updates and bug fixes

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -10,6 +10,14 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
+# Generate a personal access token with repo and admin:repo_hook
+#   permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
+
 # Deploys static resources
 echo -e "\n\n========== Deploying setup.yml =========="
 aws cloudformation deploy \
@@ -21,6 +29,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
 
+
 # Deploy the CloudFormation template
 echo -e "\n\n========== Deploying main.yml =========="
 aws cloudformation deploy \
@@ -30,7 +39,14 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
