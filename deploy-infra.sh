d80991a0a6e7f39a408e9a97a92cba6cbe8f009c
Andrew Lindsay <andrew.lindsay@gmail.com>
2020-12-13 18:26:13 +1100

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,7 +6,22 @@ CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t3a.nano
 
-# Deply the CloudFormation template
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n========== Deploying setup.yml =========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# Deploy the CloudFormation template
 echo -e "\n\n========== Deploying main.yml =========="
 aws cloudformation deploy \
   --region $REGION \
@@ -15,8 +30,7 @@ aws cloudformation deploy \
   --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    EC2InstanceType=$EC2_INSTANCE_TYPE
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
